4d7f0a9c07e7980c216a02d4d9d8f63b0d987162
sukhdeep <sukhdeepkumar918@gmail.com>
2024-07-09 14:41:07 +0530

    modified

--- appleclone/src/components/Navbar.jsx
+++ appleclone/src/components/Navbar.jsx
@@ -1,7 +1,27 @@
+import { appleImg, bagImg, searchImg } from "../utils";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center bg-slate-50">
+          <nav>  
+              <img src={appleImg} alt="Apple" width={14} height={18}/>
+
+              <div>
+                  {["Phones", "Macbooks", "Tablets"].map((nav)=>(
+                    <div key={nav}>
+                        
+                        {nav}
+
+                    </div>
+                    
+                  ))}
+              </div>
+              <div>
+                  <img src={searchImg} alt="Search"/>
+                  <img src={bagImg} alt="Cart"/>
+              </div>
+          </nav>
+     </header>
   )
 }
 
